42345921a3dc8246c1f5c1f100d441249277611e
ndstrzz <andynilessim05@gmail.com>
2025-10-07 00:30:54 +0800

    touched the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { ToastProvider } from "./components/Toaster";
+import { AuthProvider } from "./state/AuthContext";       // ⬅️ add this
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
+      <AuthProvider>                                     {/* ⬅️ wrap the app */}
+        <ToastProvider>
+          <App />
+        </ToastProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
